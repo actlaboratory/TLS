e9081ce733054c1f34455f378a58ff88ec5ba8be
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2020-08-10 19:16:35 +0900

    過去ライブの録画に対応

--- saver.py
+++ saver.py
@@ -5,9 +5,19 @@ import os
 import subprocess
 import twitcasting.twitcasting
 import datetime
+import requests
+import re
 
 class Saver:
 	def getHlsUrl(self, userId):
+		if "/movie/" in userId:
+			self.movieInfo = twitcasting.twitcasting.GetMovieInfo(userId[userId.rfind("/") + 1:])
+			response = requests.get("http://twitcasting.tv/%s" %(userId)).text
+			start = re.search("https:\\\/\\\/dl\d\d\.twitcasting\.tv\\\/tc\.vod\\\/", response).start()
+			end = response.find("\"", start)
+			url = response[start:end]
+			url = url.replace("\\/", "/")
+			return url
 		self.movieInfo = twitcasting.twitcasting.GetCurrentLive(userId)
 		try:
 			return self.movieInfo["movie"]["hls_url"]
@@ -20,6 +30,8 @@ class Saver:
 		elif "http://twitcasting.tv/" in userId:
 			userId = userId[22:]
 		url = self.getHlsUrl(userId)
+		if "/" in userId:
+			userId = userId[0:userId.find("/")]
 		if url == False:
 			return
 		startTime = datetime.datetime.fromtimestamp(self.movieInfo["movie"]["created"])
