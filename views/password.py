565c0ad493c5fc59c93f9fc1426857334ed05ca1
yamahubuki <38341927+yamahubuki@users.noreply.github.com>
2020-09-22 10:36:21 +0900

    Viewkit更新 (#2)

--- views/password.py
+++ views/password.py
@@ -19,7 +19,7 @@ class Dialog(BaseDialog):
 	def InstallControls(self):
 		"""いろんなwidgetを設置する。"""
 		self.creator=views.ViewCreator.ViewCreator(0,self.panel,self.sizer,wx.VERTICAL,20)
-		self.iText,self.static=self.creator.inputbox(_("合い言葉を入力"),400)
+		self.iText,self.static=self.creator.inputbox(_("合い言葉を入力"),None,"",0,400)
 		self.creator=views.ViewCreator.ViewCreator(1,self.panel,self.sizer,wx.HORIZONTAL,20,"",wx.ALIGN_RIGHT)
 		self.bOk=self.creator.okbutton(_("ＯＫ"),None)
 		self.bCancel=self.creator.cancelbutton(_("キャンセル"),None)
